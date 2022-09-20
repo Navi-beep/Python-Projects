2af17b23efa7e2bfbf19341f6ae4996302d8a278
Navi-Beep <estawars30@gmail.com>
2022-09-20 17:38:50 -0500

    added fun aphex twin album thingy

---
+++ aphex_twin.py
@@ -0,0 +1,26 @@
+def aphex_twin():
+    fav_songs = []
+
+    song_name = input(f"What's your favorite Aphex Twin song?: ")
+    res = input(f"{song_name} is a great song! Do you want me to add to your favorite songs list? Select 'y' or 'n' ")
+
+
+    if res == 'y':
+        print("Ok! I'll add it your favorite list")
+        fav_songs.append(song_name)
+
+    if res == 'n':
+        print(" I won't add it to your favorite songs list!")
+        res2 = (f"would you like to see your current favorite songs? select 'y' or 'n'")
+
+        if res2 == 'y':
+                print(fav_songs)
+
+    else:
+        print(fav_songs)
+    
+
+
+
+
+aphex_twin()
