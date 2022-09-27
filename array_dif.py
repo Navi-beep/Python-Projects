62ce042eb0b0afdd94067cec3811c56acd1e5eb9
Navi-Beep <estawars30@gmail.com>
2022-09-26 20:35:25 -0500

    added file

---
+++ array_dif.py
@@ -0,0 +1,20 @@
+#implement a difference function, which subtracts one list from another and returns the result.
+
+#It should remove all values from list a, which are present in list b keeping their order.
+
+
+def array_dif(a,b):
+    c = []
+    for i in a:
+        if i in b:
+            pass
+        else:
+            c.append(i)
+    return c
+
+
+
+
+
+
+print(array_dif([2,2,2,2,4,5,6],[2,4]))
