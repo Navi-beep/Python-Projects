b7481cae2fd3be42ea46103fdf5b797850d708cd
Navi-Beep <estawars30@gmail.com>
2022-10-09 15:35:52 -0500

    added stuff

--- len_array.py
+++ len_array.py
@@ -7,3 +7,22 @@
 #Even though [1,3,5,7] is an increasing subarray, it is not continuous as elements 5 and 7 are separated by element
 #4.
 
+def len_array(arr):
+    first = 0
+    final = 0
+    #print(range(len(arr)-1))
+    for i in range(len(arr)-1):
+        #iterate through the index values of each int
+        #print(i)
+        #arr[i]
+        #print(arr)
+        if arr[i] > arr[i+1] and len(arr[first:i+1]) > final:
+            final = len(arr[first:i+1])
+            first = i+1
+    return final 
+
+
+
+
+#print(len_array([3,1,2]))
+print(len_array([1,3,5,9,7]))
--- pivot_index.py
+++ pivot_index.py
@@ -17,5 +17,29 @@
 #Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
 #Right sum = nums[4] + nums[5] = 5 + 6 = 11
 
-def pivot_index(nums):
-    pass 
+
+def pivotIndex(nums):
+	# if the list is only 1 item long, we assume that 0th index is the pivot
+    if len(nums) == 1:
+        return 0
+	# variable to track ongoing sum of left side (start at 0)
+    sum_left = 0
+	# the right side sum starts as the sum of the entire list
+    sum_right = sum(nums)
+    print(sum_right)
+    for i in range(len(nums)):
+		# the right sum is the entire remainder of the list... so subtract the current potential pivot value
+		# from right sum to get the new right sum for this number
+        sum_right -= nums[i]
+		# if the current value for everything to the left of that potential pivot == everything to the right,
+		# we found it!
+        if sum_left == sum_right:
+            return i
+		# if not, increase our left sum to include this value before moving on to our next idx
+        sum_left += nums[i]
+	# never found one? return -1
+    return -1
+
+print(pivotIndex([6]))
+print(pivotIndex([1,2,3,4,3,2,1]))
+
