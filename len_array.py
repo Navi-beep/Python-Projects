b90d4bbe96d451430201d9db39a027485b3164ba
Navi-Beep <estawars30@gmail.com>
2022-10-08 20:54:28 -0500

    added file

---
+++ len_array.py
@@ -0,0 +1,9 @@
+#You are given an array of integers arr, return the length of the longest continuous increasing subarray.
+#A continuous increasing subarray is defined as 2 or more consecutive indices such that arr[i] < arr[i+1].
+
+#Input: arr = [1,3,5,4,7]
+#Output: 3
+#Explanation: The longest continuous increasing subarray is [1,3,5] with length 3.
+#Even though [1,3,5,7] is an increasing subarray, it is not continuous as elements 5 and 7 are separated by element
+#4.
+
