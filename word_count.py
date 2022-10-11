b74369ad086480f72a5417698801ad0fb1ce3ee4
Navi-Beep <estawars30@gmail.com>
2022-10-11 17:42:26 -0500

    added file

--- word_count.py
+++ word_count.py
@@ -4,4 +4,9 @@ print(cats.upper())
 
 print('cheese kind of smell like cheese'.count('cheese'))
 
+#count number of words in a given string
+print('this is a string of cheese look at how marvelous it is.'.count(' ') + 1)
 
+line = input()
+word_total = line.count(' ') + 1 
+print(word_total)
