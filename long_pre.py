b32fe7b5d2870a21be816dcd8c34a078c95f5158
Navi-Beep <estawars30@gmail.com>
2022-10-12 10:56:25 -0500

    added file

---
+++ long_pre.py
@@ -0,0 +1,44 @@
+#Write a function to find the longest common prefix string amongst an array of strings.
+
+#If there is no common prefix, return an empty string "".
+
+ 
+
+#Example 1:
+
+#Input: strs = ["flower","flow","flight"]
+#Output: "fl"
+
+
+def long_pre(strs):
+       
+        if not strs:
+            return ""
+
+        smallest = min(strs, key=len)
+        for i, x in enumerate(smallest):
+            for other in strs:
+                if other[i] != x:
+                    return smallest[:i]
+        return smallest
+
+
+print(long_pre(["flower","flow","flight"]))
+
+def longest_Prefix(b):
+        
+        prefix = b[0]
+        #print(prefix)
+        
+        for i in b:
+            #print(i)
+            while not i.startswith(prefix):
+                prefix = prefix[:-1]
+                print()
+                #print(prefix)
+        
+        return prefix
+
+print(longest_Prefix(["flower","flow","flight"]))
+
+    
