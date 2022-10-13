db051eb398deec5183dbc2dbe0b1d8e0cc7952e0
Navi-Beep <estawars30@gmail.com>
2022-10-12 19:49:01 -0500

    made star wars thingy

---
+++ input.py
@@ -0,0 +1,20 @@
+#Put in a character from Star Wars and get a quote! Have fun! 
+
+cats = input("Hello there! Name your favorite Star Wars character and I'll give you a quote, have fun!!!: ")
+while True:
+    if cats == "General Kenobi" or "general kenobi":
+        print('Hello there General Kenobi')
+        continue
+        
+
+    elif cats == "Stormtrooper" or "stormtrooper":
+        print("These aren't the droids you're looking for. Move along now.")
+
+    elif cats == "Luke Skywalker" or "luke skywalker":
+        print('Luke, I am your father')
+
+    elif cats == "Luke Skywalker":
+        print('Luke, I am your father')
+
+    elif cats == "Luke Skywalker":
+        print('Luke, I am your father')
