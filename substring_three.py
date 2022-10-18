44d2ad044e367e785de0da1682ac200e63078460
Navi-Beep <estawars30@gmail.com>
2022-10-18 16:38:20 -0500

    changed file

--- substring_three.py
+++ substring_three.py
@@ -17,6 +17,7 @@
 def countGoodSubstrings(s):
         counter = 0
         for i in range(len(s)):
+            #making the string into a set, removes the duplicates, so if there are 3 in a set, it means its unique as there are no duplicates within the set. 
             if len(set(s[i:i+3])) == 3:
                 counter += 1
         return counter
