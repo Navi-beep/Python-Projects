48ce2a94c37597700631ee32e870f37ef527802f
Navi-Beep <estawars30@gmail.com>
2022-10-31 15:13:42 -0500

    added file

---
+++ space_ints.py
@@ -0,0 +1,19 @@
+#Given a string of space separated integers
+#Return the highest and lowest as a space separated string "-5 83"
+#bonus challenge: do so for strings of numbers with random extra whitespace
+#Example:
+#Input: "32 27 83 25 5 -5 0 32"
+#Output: "-5 83"
+
+#Bonus Example:
+#Input: "     82  1 0  -5 32  7  14  22   "
+#Output: "-5 82"
+
+def spaced_int(string):
+    thing = string.split()
+    return min(thing) + ' ' + max(thing)
+
+
+
+
+print(spaced_int("32 27 83 25 5 -5 0 32"))
