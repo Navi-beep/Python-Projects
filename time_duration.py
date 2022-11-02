3c697d02b79af531616458d98aa7baf4ce8e88d4
Navi-Beep <estawars30@gmail.com>
2022-11-01 20:36:30 -0500

    added file

---
+++ time_duration.py
@@ -0,0 +1,8 @@
+#Readable Time Durations
+#Given a string input seconds, return a readable string of time featuring years, days, hours, minutes, and seconds.
+
+#Example:
+#Input: "31600000"
+#Output:  "1 year, 0 days, 17 hours, 46 minutes, and 40 seconds"
+#Note: Correct pluralization matters (1 year not 1 years)
+
