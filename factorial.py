617900cd1da4e701505179253a0361e373a113e7
Navi-Beep <estawars30@gmail.com>
2022-11-11 22:32:35 -0600

    made changes

--- factorial.py
+++ factorial.py
@@ -1,10 +1,11 @@
 def iterative_factorial(n):
-    fact = 1
+    factorial = 1
 
     for i in range(2, n + 1):
-        fact *= i 
+        print(i)
+        factorial *= i 
 
-    return fact
+    return factorial 
 
 
 print(iterative_factorial(5))
