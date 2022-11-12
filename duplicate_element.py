fcd1eff0369fb9c5cd8e2bfe5e7f3d7ba980d673
Navi-Beep <estawars30@gmail.com>
2022-11-11 22:29:00 -0600

    added comments

--- duplicate_element.py
+++ duplicate_element.py
@@ -2,6 +2,14 @@
 
 #You must implement a solution with a linear runtime complexity and use only constant extra space.
 
+#An algorithm has linear complexity if the time taken increases linearly with the increase in the number of inputs. 
+
+#O(1) Complexity: We consider constant space complexity when the program doesn’t contain any loop, recursive function, or call to any other functions. 
+
+#O(n) Complexity: We consider the linear space complexity when the program contains any loops. 
+
+
+
 from collections import Counter 
 
 #Example 1:
