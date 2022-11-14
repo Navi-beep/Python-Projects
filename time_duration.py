30392323d145ae8c684e5c33704e3e8a559094b3
Navi-Beep <estawars30@gmail.com>
2022-11-14 15:54:22 -0600

    added days

--- time_duration.py
+++ time_duration.py
@@ -6,3 +6,12 @@
 #Output:  "1 year, 0 days, 17 hours, 46 minutes, and 40 seconds"
 #Note: Correct pluralization matters (1 year not 1 years)
 
+def duration(s):
+    minutes = s//60
+    seconds = minutes*60
+    hours = minutes//60
+    days = hours//24
+    year = days //365.25
+    return f"That time is:{year} years, {days} days, {hours} hours, and {minutes} minutes, {seconds} seconds."
+
+print(duration(60000))
