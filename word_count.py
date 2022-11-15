fd362ab67d67e15aa04413b35861f2669034f64b
Navi-Beep <estawars30@gmail.com>
2022-11-15 15:47:54 -0600

    made changes

--- word_count.py
+++ word_count.py
@@ -10,3 +10,19 @@ print('this is a string of cheese look at how marvelous it is.'.count(' ') + 1)
 line = input()
 word_total = line.count(' ') + 1 
 print(word_total)
+
+
+def count_words(string):
+    count = 0
+    new_string = string.split()
+    for s in new_string:
+        count += 1
+    return count
+
+    
+
+    
+
+
+
+print(count_words('cats are cool'))
