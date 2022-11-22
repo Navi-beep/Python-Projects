82495ae2b7bf93cfbff65098a83cc4f6d9860d4e
Navi-Beep <estawars30@gmail.com>
2022-11-21 20:43:04 -0600

    added new file

---
+++ sort_nums.py
@@ -0,0 +1,20 @@
+#Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+#For example:
+
+#solution([1,2,3,10,5]) # should return [1,2,3,5,10]
+#solution(None) # should return []
+
+#doesnt work so far
+def solution(nums):
+        nums_2 = list(nums)
+        
+        if len(nums_2) == 0:
+            return nums_2
+        else:
+            nums_3 = nums_2.sort()
+            return nums_3
+        
+
+
+print(solution([1,2,3,7,6,8]))
