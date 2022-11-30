51b14733099b63a24ec81eb4177a36e95278e2ce
Navi-Beep <estawars30@gmail.com>
2022-11-29 20:11:35 -0600

    added file

---
+++ duplicate.py
@@ -0,0 +1,20 @@
+#Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+#There is only one repeated number in nums, return this repeated number.
+
+#You must solve the problem without modifying the array nums and uses only constant extra space.
+
+ 
+
+#Example 1:
+
+#Input: nums = [1,3,4,2,2]
+#Output: 2
+
+def find_dup(nums):
+    return nums
+    
+
+
+
+print(find_dup([1,2,2,3,4]))
