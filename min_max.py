1ff9fb5cfa0edb1fce140e4fa25d13e628b4c500
Navi-Beep <estawars30@gmail.com>
2022-12-09 21:13:01 -0600

    added file

---
+++ min_max.py
@@ -0,0 +1,13 @@
+#Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+
+def minimum(arr):
+    #minimum = minimum
+    return min(arr)
+    
+
+def maximum(arr):
+    return max(arr)
+
+arr = [100,-1111,3,45,6,7,5656]
+print(minimum(arr))
+print(maximum(arr))
