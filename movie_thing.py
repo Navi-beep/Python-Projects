6fc48321d9534c031502452984ca75f63c4f6579
Navi-Beep <estawars30@gmail.com>
2022-12-14 21:55:28 -0600

    made change

--- movie_thing.py
+++ movie_thing.py
@@ -1,6 +1,6 @@
 import random 
 
-movies = ['zoolander 2', 'harold and kumar go to white castle', 'inglorious basterds','step brothers', 'bruno', 'shaun of the dead','the dictator', 'la bamba']
+movies = ['zoolander 2', 'harold and kumar go to white castle', 'inglorious basterds','step brothers', 'bruno', 'shaun of the dead','the dictator', 'la bamba', 'family guy', 'king of the hill']
 
 
 def pick_one(movies):
