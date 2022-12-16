28b91df57968455b96f369314844dfb0c8eb2836
Navi-Beep <estawars30@gmail.com>
2022-12-16 00:54:01 -0600

    added counter

--- persistent.py
+++ persistent.py
@@ -6,6 +6,10 @@
 #999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
 #4 --> 0 (because 4 is already a one-digit number)
 
+# add a count somewhere in this
+
+from collections import Counter 
+
 def persistence(n):
     thing =[]
     for num in str(n):
