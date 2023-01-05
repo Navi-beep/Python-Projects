d54f410f72e87c5ffa417d1a9a4b5702a4b81234
Navi-Beep <estawars30@gmail.com>
2023-01-04 22:40:43 -0600

    added func

---
+++ add_length.py
@@ -0,0 +1,32 @@
+#What if we need the length of the words separated by a space to be added at the end of that same word and have it returned as an array?
+
+#Example(Input --> Output)
+
+#"apple ban" --> ["apple 5", "ban 3"]
+
+
+def words(string):
+    print(type(string))
+    new_list = []
+    for item in string:
+        new_list.append(item)
+        new_list.append(len(item))
+    
+    return new_list 
+
+
+print(words(['cats', 'noodles']))
+
+
+def add_length(str):
+    strang = str.split()
+    new_list2 = []
+    for item in strang:
+        new_list2.extend([item, len(item)])
+        
+        
+    return new_list2
+
+
+print(add_length('cats, birds'))
+
