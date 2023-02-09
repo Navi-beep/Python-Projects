26dbb9775c8f73e5b6f81eeff7e1db0bb7bd859a
Navi-Beep <estawars30@gmail.com>
2023-02-08 19:26:17 -0600

    made  change

--- reverser.py
+++ reverser.py
@@ -5,13 +5,17 @@ def reverser(num):
     rm = 0
     while num != 0:
         #remainder of num divided by 10 is stored as d (now it contains last digit)
+        
         d = num % 10
         #print(d)
         #d is then added to the var reversed after multiplying it by 10. Multiplying it by 10 added a new place in the rm. 
         rm = rm * 10 + d
-        num //= 10
+        num = num // 10
+        
 
     return rm 
 
 
-print(reverser(542))
+print(reverser(1234))
+print(1%10)
+print(1234//10)
